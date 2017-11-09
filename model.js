923b14b3b35832edf13c3022aab46d49493e7519
ZuoJinQuan <zuojinquan@ihealthlabs.com.cn>
2017-11-09 19:07:51 +0800

    add model.js

---
+++ model.js
@@ -0,0 +1,18 @@
+const Users = {
+    ID: String,//id
+    username: String,//用户名
+    psw: String,//需要加密
+    createTime: Date,//注册时间
+    lastChangeTime: Date,//更新时间
+    nickname: String,//昵称
+    avatar: String,//用户头像地址
+    gender: Number,//性别
+    // tags:Array,
+};
+const chatRooms = {
+    ID: String,//id
+    name: String,//聊天室名
+    createTime: Date,//注册时间
+    // tags: Array,
+
+};
